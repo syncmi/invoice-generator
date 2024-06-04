59ff2dae1201e690ef3448b5400caa0ab4fe3a0e
syncmi <syncmiusercare@gmail.com>
2024-06-04 21:30:50 +0530

    Font included for invoice

--- assets/js/free-invoice.js
+++ assets/js/free-invoice.js
@@ -325,14 +325,14 @@ function printInvoice() {
   var cell = document.getElementById("change-colspan");
   cell.colSpan = 2;
 
-  var terms = document.getElementById('invoice-terms').innerText;
-  if(terms==''){
-    document.getElementById('invoice-terms-h').remove();
+  var terms = document.getElementById("invoice-terms").innerText;
+  if (terms == "") {
+    document.getElementById("invoice-terms-h").remove();
   }
-  
-  var notes = document.getElementById('invoice-note').innerText;
-  if(notes==''){
-    document.getElementById('invoice-note-h').remove();
+
+  var notes = document.getElementById("invoice-note").innerText;
+  if (notes == "") {
+    document.getElementById("invoice-note-h").remove();
   }
 
   var preContent = `<!DOCTYPE html>
@@ -340,6 +340,12 @@ function printInvoice() {
      <head>
        <meta charset="utf-8" />
        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+       <!-- Google Fonts -->
+        <link href="https://fonts.gstatic.com" rel="preconnect" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+          rel="stylesheet"
+        />
        <!-- Vendor CSS Files -->
        <link
          href="assets/vendor/bootstrap/css/bootstrap.min.css"
@@ -372,7 +378,11 @@ function printInvoice() {
 
   // Remove the iframe
   setTimeout(function () {
-    window.location.reload();
+    var con = confirm("Reload in process!");
+    if (con) {
+      location.reload();
+    } else {
+      location.reload();
+    }
   }, 1000);
-  
 }
